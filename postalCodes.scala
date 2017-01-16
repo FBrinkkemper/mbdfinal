c235b0d5a605be6820ba5586aa993df343e9b0a4
Jochem Verburg <jochemverburg@gmail.com>
2017-01-16 20:23:30 +0100

    Filtered postal codes

--- postalCodes.scala
+++ postalCodes.scala
@@ -8,6 +8,7 @@ import org.apache.spark.sql.magellan.dsl.expressions._
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import scala.collection.immutable.ListMap
 
 // uncomment if your program uses sql
 import org.apache.spark.sql.{ SQLContext }
@@ -15,6 +16,10 @@ import org.apache.spark.sql.{ SQLContext }
 object postalCodes {
 
   case class TwitterRecord(userId: String, point: Point)
+  case class PostalCodesList(userId: String, postalCodes: Iterable[String])
+  case class PostalCodesMap(userId: String, postalCodes: Map[String, Int])
+  case class PostalCodesListMap(userId: String, postalCodes: ListMap[String, Int])
+  case class PostalCode(userId: String, postalCode: String)
 
   /*
    add actual program here, start by specifying
@@ -50,18 +55,46 @@ object postalCodes {
 
 		/* Determine postalcode for each user (userId, postalCode)
 		TODO Filer postalcodes */
-		val user_with_postalcode = joined
+    //.map(x => (x._0, List(x._1))) .map(lambda x : getBestCoordinate(x))
+
+		val user_with_postalcode = joined.rdd.map(x => (x(1), x(0))).groupByKey()
+      .map(x => Row(x._2.groupBy(identity).mapValues(_.size).maxBy(_._2)._1, x._1)).cache()
+      /*
+
+      .map(x => PostalCodesList(x(0).asInstanceOf[String], x(1).asInstanceOf[Iterable[String]]))
+      .map(x => PostalCodesMap(x.userId, x.postalCodes.groupBy(identity).mapValues(_.size)))
+      .map(x => PostalCodesListMap(x.userId, ListMap(x.postalCodes.toSeq.sortWith(_._2 > _._2):_*)))
+      .map(x => Row(x.userId, x.postalCodes.head._1))
+
+      map{pair =>
+      val userId = pair(0)
+      val list = pair(1).asInstanceOf[Iterable[String]]
+      val map = list.groupBy(identity).mapValues(_.size)
+      val listMap = ListMap(map.toSeq.sortWith(_._2 > _._2):_*)
+      Row(userId, listMap.head._1)
+}.cache()*/
+
+/*
+val pair = Row(x)
+
+
+val map = list.groupBy(identity).mapValues(_.size)
+val listMap = ListMap(map.toSeq.sortWith(_._2 > _._2):_*)
+Row(userId, listMap.head._1)*/
+
+    //return user_with_postalcode.repartition(1)
     //.groupBy($"userId").map{ x => return getPostalCode(x)}
 
-		/* Count users per postalcode */
-		val users_per_postalcode = user_with_postalcode.
-		groupBy($"postalcode").
+    /*groupBy($"postalcode").
 		agg(countDistinct("userId").
 		as("#users")).
-		orderBy(col("#users").desc).
-		cache()
+		orderBy(col("#users").desc)*/
 
-		return users_per_postalcode.rdd.repartition(1)
+		/* Count users per postalcode */
+		val users_per_postalcode = user_with_postalcode.map(x => (x(0), 1)).reduceByKey((x, y) => x + y).map(x => Row(x))
+		.cache()
+
+		return users_per_postalcode.repartition(1)
   }
 
   def main(args: Array[String]) {
